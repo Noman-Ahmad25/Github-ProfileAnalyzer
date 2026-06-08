aa5682e82e860d058bbec57e9929c2168bbe87b1
Noman Ahmad <nomanahmad9356@gmail.com>
2026-06-08 11:17:50 +0530

    Modify database sync behavior based on environment

--- src/config/db.ts
+++ src/config/db.ts
@@ -22,7 +22,8 @@ export const db = new Sequelize(connectionString, {
 export const connectDB = async (): Promise<void> => {
   try {
     await db.authenticate();
-    await db.sync({ alter: true });
+    const isProd = process.env.NODE_ENV === 'production' || process.env.RENDER === 'true';
+    await db.sync({ alter: !isProd })
     console.log("MySQL Database Connection Pool Verified successfully!");
   } catch (error: any) {
     console.error("MySQL Connection Failed:", error.message);
